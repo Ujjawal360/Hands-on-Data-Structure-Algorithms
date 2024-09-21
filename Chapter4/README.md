f08206d3a5b4e08ec2c1faa4ac9327aa5c2e01eb
Ujjawal Shah <ujjawalshah360@gmail.com>
2024-09-21 19:16:42 +0000

    insert at particular index position

--- Chapter4/README.md
+++ Chapter4/README.md
@@ -20,3 +20,7 @@ This implementation of adding the element to the end of the list of **O(1)** bec
   * if no element is in the linkedList, the head and tail becomes the same
   * **_While adding the element to the tail, the new tail will shift too (to the node just created)._**
 
+
+* Inserting value at the particular index: [Insert at particuar index](insertAtParticularIndex.py) <br>
+  * Remember that **_temp = curr.next_** will store all the values that comes after curr.next
+  ![Adding at the end](temp=curr.next.png)
---
+++ Chapter4/insertAtParticularIndex.py
@@ -0,0 +1,58 @@
+class Node:
+  def __init__(self, val):
+    self.val = val
+    self.next = None
+
+class LinkedList:
+  def __init__(self):
+    self.head = None
+    self.tail = None
+    self.length = 0
+
+  def append(self, val):
+    newNode = Node(val)
+    if not self.head and not self.tail:
+      self.head = newNode
+      self.tail = newNode
+      self.length += 1
+    else:
+      self.tail.next = newNode
+      self.tail = newNode
+      self.length += 1
+
+  def insertAtIndex(self, value, index):
+    if index < 0 or index > self.length:
+      return "It cannot be done"
+
+    if index == 0:
+      newNode = Node(value)
+      newNode.next = self.head
+      self.head = newNode
+      self.length += 1
+    else:
+      curr = self.head
+      count = 0
+      while curr:
+        if count == index - 1:
+          temp = curr.next
+          newNode = Node(value)
+          newNode.next = temp
+
+          curr.next = newNode
+          self.length += 1
+          count += 1
+        else:
+          count += 1
+          curr = curr.next
+  def print(self):
+    current = self.head
+    while current != None:
+      print(f"{current.val} --> ", end=" ")
+      current = current.next
+
+my_list = LinkedList()
+my_list.append(1)
+my_list.append(2)
+my_list.append(3)
+my_list.insertAtIndex(4, 0)
+my_list.print()
