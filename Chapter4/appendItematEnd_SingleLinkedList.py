77e2e3bec518afec8b4560ac7afaac7e4529ef76
Ujjawal Shah <ujjawalshah360@gmail.com>
2024-09-21 17:34:19 +0000

    formatting of the code

--- Chapter4/appendItematEnd_SingleLinkedList.py
+++ Chapter4/appendItematEnd_SingleLinkedList.py
@@ -1,13 +1,11 @@
 # adding item to the end of the linked List
 class Node:
-
   def __init__(self, val):
     self.val = val
     self.next = None
 
 
 class LinkedList:
-
   def __init__(self):
     self.head = None
 
--- Chapter4/appendItematEnd_SingleLinkedList_tail.py
+++ Chapter4/appendItematEnd_SingleLinkedList_tail.py
@@ -1,12 +1,10 @@
 class Node:
-
   def __init__(self, val):
     self.val = val
     self.next = None
 
 
 class LinkedList:
-
   def __init__(self):
     self.head = None
     self.tail = None
@@ -25,7 +23,6 @@ class LinkedList:
     while current != None:
       print(f"{current.val} --> ", end=" ")
       current = current.next
-
 
 my_list = LinkedList()
 my_list.append(1)
