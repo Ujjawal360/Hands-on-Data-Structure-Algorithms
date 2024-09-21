3f5428e8c425cf00021558e5e80e34a95c3067b4
Ujjawal Shah <ujjawalshah360@gmail.com>
2024-09-21 16:55:43 +0000

    - Adding item at end of Single LInked list

---
+++ Chapter4/README.md
@@ -0,0 +1,12 @@
+This chapter is on LinkedList.
+
+
+The topics are:
+* Single Linked List
+* Double linked list
+* Circular linked list
+
+We will see Single linked list first:
+
+* Inserting value at the end (append operation): [Adding item at end Single Linked List](Chapter4/appendItematEnd_SingleLinkedList.py)
+
---
+++ Chapter4/appendItematEnd_SingleLinkedList.py
@@ -0,0 +1,34 @@
+class Node:
+
+  def __init__(self, val):
+    self.val = val
+    self.next = None
+
+
+class LinkedList:
+
+  def __init__(self):
+    self.head = None
+
+  def append(self, val):
+    newNode = Node(val)
+    if self.head == None:
+      self.head = newNode
+    else:
+      current = self.head
+      while current.next != None:
+        current = current.next
+      current.next = newNode
+
+  def print(self):
+    current = self.head
+    while current != None:
+      print(f"{current.val} --> ", end=" ")
+      current = current.next
+
+
+my_list = LinkedList()
+my_list.append(1)
+my_list.append(2)
+my_list.append(3)
+my_list.print()
